77eb31c0a83d83b5de098ed7ba0fa51f00c98dde
NilamKarande <nilamkarande3010@gmail.com>
2024-12-11 11:56:20 -0800

    Added skill section

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,18 +1,43 @@
 import React from 'react';
+import './skills.css';
+
+// Importing logos
+import pythonLogo from './images/python.png';
+import reactLogo from './images/react.png';
+import htmlLogo from './images/html.png';
+import cssLogo from './images/css.png';
+import jsLogo from './images/javascript.png';
+import bootstrapLogo from './images/bootstrap.png';
+import firebaseLogo from './images/firebase.png';
+import postmanLogo from './images/postman.png';
+import githubLogo from './images/github.png';
+
 
 function Skills() {
+  const skills = [
+    { name: 'HTML', logo: htmlLogo },
+    { name: 'CSS', logo: cssLogo },
+    { name: 'JavaScript', logo: jsLogo },
+    { name: 'Bootstrap', logo: bootstrapLogo },
+    { name: 'React.js', logo: reactLogo },
+    { name: 'Firebase', logo: firebaseLogo },
+    { name: 'Python (Flask)', logo: pythonLogo },
+    { name: 'Postman API', logo: postmanLogo },
+    { name: 'GitHub', logo: githubLogo },
+
+  ];
+
   return (
-    <section>
-      <h2>Skills</h2>
-      <ul>
-        <li>Python (Flask)</li>
-        <li>JavaScript (React.js)</li>
-        <li>Postman API</li>
-        <li>GitHub for version control</li>
-        <li>Machine Learning: DistilBERT, NLP</li>
-        <li>Databases: BigQuery, Datastore</li>
-        <li>Frontend Optimization Techniques</li>
-      </ul>
+    <section className="skills-section">
+      <h2 className="skills-heading">Skills</h2>
+      <div className="skills-container">
+        {skills.map((skill, index) => (
+          <div className="skill-card" key={index}>
+            <img src={skill.logo} alt={`${skill.name} logo`} className="skill-logo" />
+            <p className="skill-name">{skill.name}</p>
+          </div>
+        ))}
+      </div>
     </section>
   );
 }
